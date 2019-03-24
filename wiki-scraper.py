7435c31eb9403f31c036defdaa228aa8b169f7ba
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-03-23 21:45:27 -0700

    found out we need to do it like example probably

---
+++ wiki-scraper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+data = requests.get(
+    'https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3#Uses_and_applications')
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+data = []
+
+for country_list in soup.find_all('div', {'class': 'plainlist'}):
+    for country_list_item in country_list.find_all('ul'):
+        for li in country_list_item.find_all('li'):
+            country_code = li.find_all('span')[0].text.strip()
+            country_name = li.find_all('a')[0].get('title')
+            item = {'code': country_code, 'name': country_name}
+            data.append(item)
+            
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(data)
+
+    
